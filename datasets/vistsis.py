927ecfc8dcc3250761f07dbbda04278cd1e56adb
reham salah <113318157+rehammsalah@users.noreply.github.com>
2023-03-27 04:58:50 +0200

    bbbb

--- datasets/vistsis.py
+++ datasets/vistsis.py
@@ -23,8 +23,8 @@ class StoryDataset(Dataset):
 
         self.augment = transforms.Compose([
             transforms.ToPILImage(),
-            transforms.Resize(128),
-            transforms.RandomCrop(128) if self.subset == 'train' else transforms.CenterCrop(128),
+            transforms.Resize(64),
+            transforms.RandomCrop(64) if self.subset == 'train' else transforms.CenterCrop(64),
             transforms.ToTensor(),
             transforms.Normalize([0.5], [0.5])
         ]) if self.subset in ['train', 'val'] else transforms.Compose([
